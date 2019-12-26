b531c42fd8116a95b53913f76430119eac33df36
herick <herickcwilke@gmail.com>
2019-12-26 09:39:40 -0300

    Projeto cadastro

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Cadastro de Produtos</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        body{
+            padding: 20px;
+        }
+        .navbar{
+            margin-bottom: 20px;
+        }
+    </style>
+    </head>
+    <body>
+        <div class="container">
+            @component('component_navbar')
+            @endcomponent
+            <main role="main">
+                @hasSection ('body')
+                    @yield('body')  
+                    
+                @endif
+            </main>
+        </div>
+
+    <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
